2c4edc7af4e88296799e5c1ef0d812e572e0a62f
Thiago-cordeiro <thiagocordeiro2512@gmail.com>
2025-06-04 19:02:58 -0300

    arquivo .h add

--- historico.h
+++ historico.h
@@ -1,5 +1,24 @@
 #ifndef HISTORICO_H
 #define HISTORICO_H
 
+typedef struct Atendimento{
+
+    char psicologo[100];
+    char data[10];
+    char resumo[500];
+
+} Atendimento;
+
+
+typedef struct Historico {
+
+    Atendimento atendimento; 
+    struct Historico* proximo; 
+    
+} Historico; 
+
+Historico* inserirNoHistorico(Historico* h, Atendimento novoAtendimento);
+void HistoricoPorCurso(Historico* h);//printa o historico por curso
+void HistoricoAtendimentoCPF(Historico* h);//Printa historico por cpf
 
 #endif
